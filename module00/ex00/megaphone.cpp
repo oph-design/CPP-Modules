3debf4d30d3effb38baae104fe61c049d1d64bbd
oph-design <ole.paul.heinzelmann@gmail.com>
2023-05-10 13:21:01 +0200

    implement megaphone

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oheinzel <oheinzel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 11:15:37 by oheinzel          #+#    #+#             */
+/*   Updated: 2023/05/10 13:03:58 by oheinzel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string.h>
+
+int	main(int argc, char **argv)
+{
+	unsigned int	i = -1;
+	unsigned int	j = -1;
+
+	if (argc == 1)
+		return (std::cout << "* LOUD UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
+	while (argv[++i])
+	{
+		while (argv[i][++j])
+			argv[i][j] = toupper(argv[i][j]);
+		j = -1;
+	}
+	i = 1;
+	while (argv[i])
+		std::cout << argv[i++];
+	std::cout << "\n";
+	return (0);
+}
