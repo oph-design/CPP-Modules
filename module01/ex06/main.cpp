7cf7837baa3d52fa0c3bb0c2795c34f2d7657987
oph-design <ole.paul.heinzelmann@gmail.com>
2023-05-15 08:54:00 +0200

    fix m01 ex06

--- module01/ex06/main.cpp
+++ module01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oheinzel <oheinzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 20:54:03 by oheinzel          #+#    #+#             */
-/*   Updated: 2023/05/14 21:28:39 by oheinzel         ###   ########.fr       */
+/*   Updated: 2023/05/15 08:53:18 by oheinzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,16 @@ int main(int argc, char **argv)
 	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
 	if (argc != 2)
-		return (std::cerr << "[ Wrong number of args ]\n", 0);
+		i = 4;
 	while (i < 4 && std::string(argv[1]).compare(levels[++i]));
 	switch (i)
 	{
 		case 0:
 			harl.complain("DEBUG");
-			harl.complain("INFO");
-			harl.complain("WARNING");
-			harl.complain("ERROR");
-			break;
 		case 1:
 			harl.complain("INFO");
-			harl.complain("WARNING");
-			harl.complain("ERROR");
-			break;
 		case 2:
 			harl.complain("WARNING");
-			harl.complain("ERROR");
-			break;
 		case 3:
 			harl.complain("ERROR");
 			break;
