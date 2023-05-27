dc07aaea80677b4692967b00dd51ba5ffe456ad1
oph-design <ole.paul.heinzelmann@gmail.com>
2023-05-27 14:39:07 +0200

    submit better version of makeForm

--- module05/ex03/src/Intern.cpp
+++ module05/ex03/src/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: oheinzel <oheinzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:52:48 by oheinzel          #+#    #+#             */
-/*   Updated: 2023/05/26 18:54:46 by oheinzel         ###   ########.fr       */
+/*   Updated: 2023/05/27 14:37:56 by oheinzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,24 @@ Intern&  Intern::operator=(const Intern& rhs) {
 }
 
 AForm* makeForm(std::string name, std::string target) {
-  if (!name.compare("robotomy request"))
-    return (std::cout << "Intern creates " << name << "\n", new RobotomyRequestForm(target));
-  if (!name.compare("presidential pardon"))
-    return (std::cout << "Intern creates " << name << "\n", new PresidentialPardonForm(target));
-  if (!name.compare("shrubbery creation"))
-    return (std::cout << "Intern creates " << name << "\n", new ShrubberyCreationForm(target));
-  std::cout << "Theres no form named " << name << "\n";
+  unsigned int i = 0;
+  AForm *res = nullptr;
+  std::string type[3] = {"robotomy request", "presidential pardon",
+                        "shrubbery creation"};
+
+  while(i < 3 && name.compare(type[i]))
+    i++;
+  switch (i)
+  {
+    case 0:
+      res = new RobotomyRequestForm(target); break;
+    case 1:
+      res = new PresidentialPardonForm(target); break;
+    case 2:
+      res = new ShrubberyCreationForm(target); break;
+    default:
+      std::cout << name << " isn't a valid form" << std::endl; return res;
+  }
+  std::cout << "Intern creates " << name << std::endl;
+  return (res);
 }
