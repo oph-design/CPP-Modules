2cec51a3bca737f421040f34cb49d4599c9d30fd
oph-design <115570424+oph-design@users.noreply.github.com>
2023-07-11 13:03:40 +0200

    Update main.cpp

--- module09/ex00/src/main.cpp
+++ module09/ex00/src/main.cpp
@@ -14,7 +14,7 @@
 
 int main(int argc, char *argv[]) {
   if (argc != 2)
-    return (std::cerr << "Error: number of inputs" << std::endl, 1);
+    return (std::cerr << "Error: could not open file" << std::endl, 1);
   try {
     BitcoinExchange BitcoinExchange(argv[1]);
     BitcoinExchange.calcBitcoinExchange();
